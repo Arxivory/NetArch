f6f26197517bb11b311c39fd1ead56acc8f51145
Vince <babyaivan86@gmail.com>
2026-02-08 18:19:45 +0800

    Fixed  undefined variable error.

--- src/core/PhysicalController.js
+++ src/core/PhysicalController.js
@@ -16,10 +16,10 @@ export class PhysicalController {
             console.log('Adding domain to physical controller: ', domain);
             const { x, y, width, height } = domain.geometry;
 
-            const geometry = new THREE.BoxGeometry(width * this.defaultScaler, 1, height * defaultScaler);
+            const geometry = new THREE.BoxGeometry(width * this.defaultScaler, 1, height * this.defaultScaler);
             const material = new THREE.MeshBasicMaterial({ color: 0x909090 });
             const mesh = new THREE.Mesh(geometry, material);
-            mesh.position.set(x * defaultScaler, 0.1, y * defaultScaler); 
+            mesh.position.set(x * this.defaultScaler, 0.1, y * this.defaultScaler); 
             this.scene.add(mesh);
         }
     }

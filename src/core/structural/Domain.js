a62f50489139f9866400f5e4813c779c69c63e65
draylao <dray.lao@gmail.com>
2026-05-01 14:41:57 +0800

    Refactor geometry properties in structural classes
    - Updated geometry properties in Domain, Floor, Site, and Space classes to use 'width' and 'height' instead of 'w', 'h', 'maxX', and 'maxY'.
    - Modified Floor and Site classes to set default values for 'type' and 'shapeType' more consistently.
    - Removed commented-out methods in Site class for better code clarity.
    - Ensured consistent handling of geometry properties across different structural components.

--- src/core/structural/Domain.js
+++ src/core/structural/Domain.js
@@ -9,10 +9,8 @@ export default class Domain {
         this.geometry = {
             x: data.x || 0,
             y: data.y || 0,
-            maxX: data.maxX || 0,
-            maxY: data.maxY || 0,
-            w: data.w || 0,
-            h: data.h || 0,
+            width: data.w || 0,
+            height: data.h || 0,
             radius: data.r || 0,
             points: data.points ? [...data.points] : []
         };
